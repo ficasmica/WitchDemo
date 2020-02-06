707518ad9d9dc817993d67fcf601ea2e0fcfe614
ficasmica <54986618+ficasmica@users.noreply.github.com>
2020-02-06 15:19:14 +0100

    blabla

---
+++ Assets/Scripts/Trees.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trees : MonoBehaviour
+{   
+    void Start(){
+        foreach (Transform child in transform){
+            child.localScale *= Random.Range(0.8f, 1.2f);
+        }
+    }
+}
